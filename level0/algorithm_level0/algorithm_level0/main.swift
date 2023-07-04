a20e4f2e0247bb9ab64d1b7b4b73a0b9c15376fc
Khyeji <khjji0502@gmail.com>
2023-07-05 00:17:36 +0900

    03.대소문자 바꿔서 출력하기

--- level0/algorithm_level0/algorithm_level0/main.swift
+++ level0/algorithm_level0/algorithm_level0/main.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+// MARK: 대소문자 바꿔서 출력하기
+// 유니코드를 이용 - A = 65, Z =90, a = 97, z = 122
+let s1 = readLine()!
+var result: String = ""
+for i in s1.unicodeScalars {
+    if i.value <= 90 {
+        result += String(i).lowercased()
+    } else {
+        result += String(i).uppercased()
+    }
+}
+print(result)
+
+// isUppercase 이용 - Character
+let s2 = readLine()!
+print(s2.map { $0.isUppercase ? $0.lowercased() : $0.uppercased() }.joined())
