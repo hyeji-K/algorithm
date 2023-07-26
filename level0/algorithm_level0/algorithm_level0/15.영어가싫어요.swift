4963c441531c0690fdf14180020ba3cf8d7551ba
Khyeji <khjji0502@gmail.com>
2023-07-26 23:39:51 +0900

    15.영어가 싫어요

--- level0/algorithm_level0/algorithm_level0/15.영어가싫어요.swift
+++ level0/algorithm_level0/algorithm_level0/15.영어가싫어요.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+func solution(_ numbers:String) -> Int64 {
+    let alphabet: [String] = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    let number: [Int] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    var result: String = ""
+    var num: String = ""
+    
+    for (_, i) in numbers.enumerated() {
+        num += String(i)
+        if alphabet.contains(num) {
+            let index = alphabet.firstIndex(of: num)!
+            result += String(number[index])
+            num = ""
+        }
+    }
+    
+    return Int64(result)!
+}
