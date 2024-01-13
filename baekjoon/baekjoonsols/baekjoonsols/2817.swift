5ac1dea9ab345a7f9e8694df73909ec20924f7ce
Khyeji <khjji0502@gmail.com>
2024-01-13 22:09:45 +0900

    백준 2817 수정

--- baekjoon/baekjoonsols/baekjoonsols/2817.swift
+++ baekjoon/baekjoonsols/baekjoonsols/2817.swift
@@ -7,48 +7,34 @@
 
 import Foundation
 
-let countOfParticient = readLine()!
-let numberOfStep = readLine()!
-var stepName: [String] = []
-var stepInfo: [Int] = []
+let participants = readLine()!
+let numberOfstaff = readLine()!
+var staffName: [String] = []
+var staffInfo: [Int] = []
 var result: [Int: String] = [:]
 var ans: [String: Int] = [:]
 
-for _ in 0..<Int(numberOfStep)! {
+for _ in 0..<Int(numberOfstaff)! {
     let input = readLine()!.components(separatedBy: " ")
-//    print(input)
-    // TODO: 득표율이 전체의 5%이상
-//    print(input[1])
-//    print(countOfParticient)
-//    print((Int(input[1])!/Int(countOfParticient)!))
-    if (Double(input[1])!/Double(countOfParticient)!) * 100 >= 5 {
-//        print("등록")
-        stepName.append(input[0])
-        stepInfo.append(Int(input[1])!)
+    // MEMO: 득표율이 전체의 5%이상인 참가자만
+    if (Double(input[1])!/Double(participants)!) * 100 >= 5 {
+        staffName.append(input[0])
+        staffInfo.append(Int(input[1])!)
     }
 }
-//print(stepInfo)
-//print(stepName)
-
+// 각 스태프들의 득표수를 1부터 14까지의 숫자로 나눠서 딕셔너리에 삽입
 for i in 1...14 {
-    for j in 0..<stepName.count {
-        let value = stepInfo[j] / i
-        result.updateValue(stepName[j], forKey: value)
-//        result[stepName[j]] = value
+    for j in 0..<staffName.count {
+        let value = staffInfo[j] / i
+        result.updateValue(staffName[j], forKey: value)
     }
 }
-//print(result)
-
+// 딕셔너리를 스태프의 점수가 큰 값부터 내림차순으로 정렬
 let sortedDictionary = result.sorted { $0.0 > $1.0 }
+// 14개의 칩을 0부터 13까지 정렬된 스태프에게 나눠줌
 for i in 0..<14 {
     if ans.contains { key, value in
         return key == sortedDictionary[i].value
-//        if key == sortedDictionary[i].key {
-//            ans[key] = value + 1
-//            return ans
-//        } else {
-//            return ans.updateValue(1, forKey: key)
-//        }
     } {
         ans[sortedDictionary[i].value] = ans[sortedDictionary[i].value]! + 1
     } else {
@@ -56,8 +42,19 @@ for i in 0..<14 {
     }
 }
 
-let printOfResult = ans.sorted { $0.0 < $1.0 }
+// 틀린 이유: 받은 득표수가 5% 이상인 스태프들 중에서도 칩을 받지 못해 0개인 스태프도 출력해야함 -> 코드 추가
+for i in 0..<staffName.count {
+    if ans.contains(where: { key, value in
+        return key == staffName[i]
+    }) {
+        continue
+    } else {
+        ans.updateValue(0, forKey: staffName[i])
+    }
+}
 
+// 스태프의 이름 순으로 정렬
+let printOfResult = ans.sorted { $0.0 < $1.0 }
 for (key, value) in printOfResult {
     print("\(key) \(value)")
 }
