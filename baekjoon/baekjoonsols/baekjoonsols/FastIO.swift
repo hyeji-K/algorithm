7222a9a5e1d860e4a2c09abfe86c5435d355c7a4
Khyeji <khjji0502@gmail.com>
2024-01-20 12:12:04 +0900

    빠른 입출력

---
+++ baekjoon/baekjoonsols/baekjoonsols/FastIO.swift
@@ -0,0 +1,78 @@
+//
+//  FastIO.swift
+//  baekjoonsols
+//
+//  Created by heyji on 2024/01/20.
+//
+
+import Foundation
+
+final class FileIO {
+    private let buffer: [UInt8]
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(try! fileHandle.readToEnd()!)+[UInt8(0)]
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() }
+        if now == 45 { isPositive.toggle(); now = read() }
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() }
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+    @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+        var now = read()
+
+        while now == 10 || now == 32 { now = read() }
+
+        let beginIndex = index-1
+
+        while now != 10,
+              now != 32,
+              now != 0 { now = read() }
+
+        return Array(buffer[beginIndex..<(index-1)])
+    }
+}
+
+
+let fIO = FileIO()
+let n = fIO.readInt()
+var array: [Int] = []
+
+for _ in 0..<n {
+    let input = fIO.readInt()
+    array.append(input)
+}
